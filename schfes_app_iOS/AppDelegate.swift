49339ac7e0ab0e230a0cba513d2574bc88b51241
FGO <fgo0910@gmail.com>
2017-10-12 02:32:43 +0900

    トップページ完成（仮）
    ニュースタイトルがidに
    ツイートボタンの機能未実装
    Tweetのテーブルビューを上にスワイプするとテーブルビュー広がる機能（やるか未定）

--- schfes_app_iOS/AppDelegate.swift
+++ schfes_app_iOS/AppDelegate.swift
@@ -23,6 +23,40 @@ extension UIImage{
         
         return newImage
     }
+    
+    func cropImage(image :UIImage, w:Int, h:Int) ->UIImage
+    {
+        // リサイズ処理
+        let origRef    = image.cgImage
+        let origWidth  = Int(origRef!.width)
+        let origHeight = Int(origRef!.height)
+        var resizeWidth:Int = 0, resizeHeight:Int = 0
+        
+        if (origWidth < origHeight) {
+            resizeWidth = w
+            resizeHeight = origHeight * resizeWidth / origWidth
+        } else {
+            resizeHeight = h
+            resizeWidth = origWidth * resizeHeight / origHeight
+        }
+        
+        let resizeSize = CGSize.init(width: CGFloat(resizeWidth), height: CGFloat(resizeHeight))
+        
+        UIGraphicsBeginImageContext(resizeSize)
+        
+        image.draw(in: CGRect.init(x: 0, y: 0, width: CGFloat(resizeWidth), height: CGFloat(resizeHeight)))
+        
+        let resizeImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        // 切り抜き処理
+        
+        let cropRect  = CGRect.init(x: CGFloat((resizeWidth - w) / 2), y: CGFloat((resizeHeight - h) / 2), width: CGFloat(w), height: CGFloat(h))
+        let cropRef   = resizeImage!.cgImage!.cropping(to: cropRect)
+        let cropImage = UIImage(cgImage: cropRef!)
+        
+        return cropImage
+    }
 }
 
 @UIApplicationMain
--- schfes_app_iOS/TopVC.swift
+++ schfes_app_iOS/TopVC.swift
@@ -14,10 +14,18 @@ import AlamofireImage
 
 class TopVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var items: JSON = []
-    var images: [UIImage] = []
+    var tweetItems: JSON = []
+    var tweetImages: [UIImage] = []
+    
+    var newsItems: [JSON] = []
+    var newsImages: [UIImage] = []
+    
     private var myButton: UIButton!
     private var tweetButton: UIButton!
+    
+    var newsLink1: String = ""
+    var newsLink2: String = ""
+    var newsLink3: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,6 +52,14 @@ class TopVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         let tButtonWidth: CGFloat = 200
         let tButtonHeight: CGFloat = 40
         
+        // -----------------------------------New'sラベルの作成-----------------------------------
+        let newsLabelY: CGFloat = 65
+        let newsLabel: UILabel = UILabel(frame: CGRect(x: 10, y:newsLabelY, width: bWidth, height: bHeight))
+        newsLabel.text = "New's"
+        newsLabel.textAlignment = NSTextAlignment.left
+        newsLabel.font = UIFont.systemFont(ofSize: 20)
+        self.view.addSubview(newsLabel)
+        
         
         // 配置する座標を定義(画面の中心).
         let posX: CGFloat = self.view.bounds.width/2 - bWidth/2
@@ -51,9 +67,112 @@ class TopVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         let tweetImage = UIImage(named: "images/setting_icon.png")?.ResizeUIImage(width: 30, height: 30)
         
-
-    
+        // ----------------------------------ニュースの画像-----------------------------------
         
+        let newsImage1X: CGFloat = UIScreen.main.bounds.size.width*1/10
+        let newsImageY: CGFloat = newsLabelY + bHeight
+        
+        let newsImage2X: CGFloat = UIScreen.main.bounds.size.width*4/10
+        
+        let newsImage3X: CGFloat = UIScreen.main.bounds.size.width*7/10
+        let newsImageWidth: CGFloat = UIScreen.main.bounds.size.width*2/10
+        let newsImageHeight: CGFloat = newsImageWidth
+        let news1: UIImageView = UIImageView(frame:  CGRect(x: newsImage1X, y: newsImageY, width: newsImageWidth, height: newsImageHeight))
+        let news2: UIImageView = UIImageView(frame:  CGRect(x: newsImage2X, y: newsImageY, width: newsImageWidth, height: newsImageHeight))
+        let news3: UIImageView = UIImageView(frame:  CGRect(x: newsImage3X, y: newsImageY, width: newsImageWidth, height: newsImageHeight))
+        
+        // 画像の角を丸くする
+        news1.clipsToBounds = true
+        news1.layer.cornerRadius = 5.0
+        news2.clipsToBounds = true
+        news2.layer.cornerRadius = 5.0
+        news3.clipsToBounds = true
+        news3.layer.cornerRadius = 5.0
+        
+        // タップを定義
+        let newsTap1 = UITapGestureRecognizer(target: self, action: #selector(self.toNewsLink1))
+        let newsTap2 = UITapGestureRecognizer(target: self, action: #selector(self.toNewsLink2))
+        let newsTap3 = UITapGestureRecognizer(target: self, action: #selector(self.toNewsLink3))
+        
+        // タップを検知できるようにする
+        news1.isUserInteractionEnabled = true
+        news2.isUserInteractionEnabled = true
+        news3.isUserInteractionEnabled = true
+        // viewにタップを登録
+        news1.addGestureRecognizer(newsTap1)
+        news2.addGestureRecognizer(newsTap2)
+        news3.addGestureRecognizer(newsTap3)
+        
+        // ----------------------------------ニュースタイトルの設定-----------------------------------
+        let newsTitleWidth: CGFloat = newsImageWidth
+        let newsTitleHeight: CGFloat = 40
+        let newsTitleY: CGFloat = newsImageY + newsImageHeight
+        let newsTitle1X: CGFloat = newsImage1X
+        let newsTitle2X: CGFloat = newsImage2X
+        let newsTitle3X: CGFloat = newsImage3X
+        let newsTitle1 = UILabel(frame: CGRect(x: newsTitle1X, y: newsTitleY, width: newsTitleWidth, height: newsTitleHeight))
+        let newsTitle2 = UILabel(frame: CGRect(x: newsTitle2X, y: newsTitleY, width: newsTitleWidth, height: newsTitleHeight))
+        let newsTitle3 = UILabel(frame: CGRect(x: newsTitle3X, y: newsTitleY, width: newsTitleWidth, height: newsTitleHeight))
+        
+        newsTitle1.textAlignment = NSTextAlignment.center
+        newsTitle2.textAlignment = NSTextAlignment.center
+        newsTitle3.textAlignment = NSTextAlignment.center
+        
+//        newsTitle1.text = "hoge1"
+//        newsTitle2.text = "hoge2"
+//        newsTitle3.text = "hoge3"
+        
+        
+        
+        //        if newsImages.count-1 > 0{
+        //            news1.image = newsImages[newsImages.count-1]
+        //        }
+
+        func setImage(response: DataResponse<Any>) ->Void {
+            let json = JSON(response.result.value ?? "")
+            let newsImageURL1 = json[json.count-1]["picture"].string
+            let newsImageURL2 = json[json.count-2]["picture"].string
+            let newsImageURL3 = json[json.count-3]["picture"].string
+            
+            Alamofire.request(newsImageURL1!).responseImage { response in
+                if let image = response.result.value {
+                    let newsImage1 = image.cropImage(image: image, w: 300, h: 300)
+                    news1.image = newsImage1
+                    newsTitle1.text = json[json.count-1]["id"].string
+                    self.newsLink1 = json[json.count-1]["link"].string!
+                }
+            }
+            Alamofire.request(newsImageURL2!).responseImage { response in
+                if let image = response.result.value {
+                    let newsImage2 = image.cropImage(image: image, w: 300, h: 300)
+                    news2.image = newsImage2
+                    newsTitle2.text = json[json.count-2]["id"].string
+                    self.newsLink2 = json[json.count-2]["link"].string!
+                }
+            }
+            Alamofire.request(newsImageURL3!).responseImage { response in
+                if let image = response.result.value {
+                    let newsImage3 = image.cropImage(image: image, w: 300, h: 300)
+                    news3.image = newsImage3
+                    newsTitle3.text = json[json.count-3]["id"].string
+                    self.newsLink3 = json[json.count-3]["link"].string!
+                }
+            }
+            
+        }
+        // JSON取得
+        let listUrl = "http://ytrw3xix.0g0.jp/app2017/feed";
+        Alamofire.request(listUrl).responseJSON(completionHandler: setImage)
+        
+
+        self.view.addSubview(news1)
+        self.view.addSubview(news2)
+        self.view.addSubview(news3)
+        self.view.addSubview(newsTitle1)
+        self.view.addSubview(newsTitle2)
+        self.view.addSubview(newsTitle3)
+        
+
         // -----------------------------------設定ボタンの追加-----------------------------------
         let button = UIBarButtonItem()
         button.image = UIImage(named: "images/setting_icon.png")?.ResizeUIImage(width: 30, height: 30)
@@ -64,17 +183,26 @@ class TopVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         // -----------------------------------See allボタンの作成-----------------------------------
         let allNewsButton = UIButton()
+        let allNewsButtonWidth: CGFloat = 100
+        let allNewsButtonHeight: CGFloat =  33
+        let allNewsButtonX: CGFloat = UIScreen.main.bounds.size.width*19/20 - allNewsButtonWidth
+        let allNewsButtonY: CGFloat = newsTitleY + newsTitleHeight
+        
         // ボタンの設置座標とサイズを設定する.
-        allNewsButton.frame = CGRect(x: posX, y: UIScreen.main.bounds.size.height*3/10, width: bWidth, height: bHeight)
+        allNewsButton.frame = CGRect(x: allNewsButtonX, y: allNewsButtonY, width: allNewsButtonWidth, height: allNewsButtonHeight)
         // ボタンの背景色を設定.
-        allNewsButton.backgroundColor = UIColor.red
+//        allNewsButton.backgroundColor = UIColor.red
         // ボタンの枠を丸くする.
-        allNewsButton.layer.masksToBounds = true
+//        allNewsButton.layer.masksToBounds = true
         // コーナーの半径を設定する.
-        allNewsButton.layer.cornerRadius = 20.0
+//        allNewsButton.layer.cornerRadius = 20.0
         // タイトルを設定する(通常時).
-        allNewsButton.setTitle(">See all", for: .normal)
-        allNewsButton.setTitleColor(UIColor.white, for: .normal)
+//        allNewsButton.setTitle(">See all", for: .normal)
+//        allNewsButton.setTitleColor(UIColor.white, for: .normal)
+        
+        // ボタンに画像を設定する
+        let seeAllImage = UIImage(named: "images/see_all.png")?.ResizeUIImage(width: 100, height: 33)
+        allNewsButton.setImage(seeAllImage, for: .normal)
         // ボタンにタグをつける.
         allNewsButton.tag = 1
         // イベントを追加する
@@ -113,18 +241,12 @@ class TopVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tweetButton.setImage(tweetImage, for: .normal)
         tweetButton.setTitleColor(UIColor.white, for: .normal)
         // ボタンにタグをつける.
-        tweetButton.tag = 1
+        tweetButton.tag = 2
         // イベントを追加する
         tweetButton.addTarget(self, action: #selector(self.Tweet), for: .touchUpInside)
         // ボタンをViewに追加.
         self.view.addSubview(tweetButton)
-        
-        // -----------------------------------New'sラベルの作成-----------------------------------
-        let newsLabel: UILabel = UILabel(frame: CGRect(x: 10, y:65, width: 100, height: bHeight))
-        newsLabel.text = "New's"
-        newsLabel.textAlignment = NSTextAlignment.left
-        newsLabel.font = UIFont.systemFont(ofSize: 20)
-        self.view.addSubview(newsLabel)
+
         
         // -----------------------------------みんなの反応ラベルの作成-----------------------------------
         let tweetLabel: UILabel = UILabel(frame: CGRect(x: 10, y: tableY-bHeight, width: 200, height: bHeight))
@@ -132,7 +254,7 @@ class TopVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tweetLabel.textAlignment = NSTextAlignment.left
         tweetLabel.font = UIFont.systemFont(ofSize: 20)
         self.view.addSubview(tweetLabel)
-        
+
         
         
         // -----------------------------------TwitterAPI関連-----------------------------------
@@ -157,16 +279,16 @@ class TopVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                 if let error = error {
                     print(error.localizedDescription)
                 } else if let data = data, let jsonString = String(data: data, encoding: .utf8) {
-                    self.items = JSON(data: data)
-//                    print(self.items)
+                    self.tweetItems = JSON(data: data)
+//                    print(self.tweetItems)
                     
-                    self.items["statuses"].forEach{(_, data) in
+                    self.tweetItems["statuses"].forEach{(_, data) in
 //                        print(data)
                         let urlString = data["user"]["profile_image_url"].string
                         // 画像取得
                         Alamofire.request(urlString!).responseImage { response in
                             if let image = response.result.value {
-                                self.images.append(image)
+                                self.tweetImages.append(image)
                             }
                             tableView.reloadData()
                         }
@@ -206,23 +328,23 @@ class TopVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 //        print(indexPath.row)
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "TableCell")
-        cell.textLabel?.text = items["statuses"][indexPath.row]["text"].string
+        cell.textLabel?.text = tweetItems["statuses"][indexPath.row]["text"].string
         cell.textLabel?.numberOfLines = 0
-        if images.count-1 >= indexPath.row {
-            cell.imageView?.image = images[indexPath.row]
+        if tweetImages.count-1 >= indexPath.row {
+            cell.imageView?.image = tweetImages[indexPath.row]
         }
-//        print(self.items["statuses"][indexPath.row])
-        print("indexPath.row : " + String(indexPath.row))
-        print("text : " + (cell.textLabel?.text!)!)
+//        print(self.tweetItems["statuses"][indexPath.row])
+//        print("indexPath.row : " + String(indexPath.row))
+//        print("text : " + (cell.textLabel?.text!)!)
 
         return cell
     }
     
     // cellの数を設定
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("items : " + String(items["statuses"].count))
-        print("images : " + String(images.count))
-        return items["statuses"].count
+//        print("tweetItems : " + String(tweetItems["statuses"].count))
+//        print("tweetImages : " + String(tweetImages.count))
+        return tweetItems["statuses"].count
     }
     
     // サイズの指定
@@ -239,11 +361,30 @@ class TopVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
 
     func Tweet() {
+        print("tweetボタンが押されました")
     }
     
     func TapMenu() {
         print("メニューがタップされました")
     }
-
+    
+    /// viewをタップされた時の処理
+    func toNewsLink1(){
+        print("1")
+        if let url = NSURL(string: newsLink1) {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    func toNewsLink2(){
+        print("2")
+        if let url = NSURL(string: newsLink2) {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    func toNewsLink3(){
+        print("3")
+        if let url = NSURL(string: newsLink3) {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
 }
-
