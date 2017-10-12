8e6cc654b112e0be1cbcf9cf1bb78824bb87ba48
FGO <fgo0910@gmail.com>
2017-10-13 01:29:30 +0900

    UserDefaultsを用いたお気に入り機能の実装

--- schfes_app_iOS/AllScheduleVC.swift
+++ schfes_app_iOS/AllScheduleVC.swift
@@ -13,12 +13,17 @@ import SwiftyJSON
 
 class AllScheduleVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var checkListItem: [String : Bool] = [:]
-    
+    // お気に入り時の画像
     let favoIcon = UIImage(named: "images/favo_icon.png")?.ResizeUIImage(width: 45, height: 45).withRenderingMode(.alwaysOriginal)
+    // 非お気に入り時の画像
     let noFavoIcon = UIImage(named: "images/no_favo_icon.png")?.ResizeUIImage(width: 45, height: 45).withRenderingMode(.alwaysOriginal)
     
+    // お気に入り登録しているかどうかの値を保持する変数
+    // アプリが落ちても値を保持できるUserDefaultsを使用
+    let userDefaults = UserDefaults.standard
     
+    // FavoScheduleVCからリロードできるようにするため、staticに設定
+    static let allScheduleTableView: UITableView = UITableView()
     
     // itemsをJSONの配列と定義
     var items: [JSON] = []
@@ -26,57 +31,57 @@ class AllScheduleVC: UIViewController, UITableViewDelegate, UITableViewDataSourc
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // TableViewを作成
-        let tableView = UITableView()
-        tableView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-        tableView.delegate = self
-        tableView.dataSource = self
-        self.view.addSubview(tableView)
-        
-//        let favo = UITapGestureRecognizer(target: self, action: #selector(self.Favo))
-//        tableView.addGestureRecognizer(favo)
+        // TableViewの設定
+        AllScheduleVC.allScheduleTableView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+        AllScheduleVC.allScheduleTableView.delegate = self
+        AllScheduleVC.allScheduleTableView.dataSource = self
+        self.view.addSubview(AllScheduleVC.allScheduleTableView)
         
         // データを取得
         let listUrl = "http://ytrw3xix.0g0.jp/app2017/timetable";
         Alamofire.request(listUrl).responseJSON{ response in
             let json = JSON(response.result.value ?? "")
             json.forEach{(_, data) in
+                // itemsに取得したデータを追加
                 self.items.append(data)
-                self.checkListItem[data["id"].string!] = false
-                print(self.checkListItem)
             }
-            tableView.reloadData()
+            // テーブルビューのリロード
+            AllScheduleVC.allScheduleTableView.reloadData()
         }
     }
     // Cellが選択された際に呼び出される
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print("Num: \(indexPath.row)")
-//        print("Value: \(items[indexPath.row])")
         if let cell = tableView.cellForRow(at: indexPath) {
-            if self.checkListItem[items[indexPath.row]["id"].string!] == true {
-                self.checkListItem[items[indexPath.row]["id"].string!] = false
-                cell.imageView?.image = noFavoIcon
-            } else {
-                self.checkListItem[items[indexPath.row]["id"].string!] = true
-                cell.imageView?.image = favoIcon
-            }
+            // cellの選択を解除する
+            // これがないとセルを選択した時グレーになり続ける
             cell.isSelected = false
         }
     }
     
     // tableのcellにAPIから受け取ったデータを入れる
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // セルの作成
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "TableCell")
+        
+        // セルのテキストの設定
         cell.textLabel?.text = items[indexPath.row]["title"].string
         cell.detailTextLabel?.text = "開催時刻 : \(items[indexPath.row]["time"].stringValue)"
         
-        if self.checkListItem[items[indexPath.row]["id"].string!] == true {
+        // セルの画像設定
+        if userDefaults.bool(forKey: items[indexPath.row]["id"].string!) == true {
             cell.imageView?.image = favoIcon
         } else {
             cell.imageView?.image = noFavoIcon
         }
-        //        cell.textLabel?.text = items["timetable"][indexPath.row]["title"].string
-        //        cell.detailTextLabel?.text = "投稿日 : \(items[indexPath.row].stringValue)"
+        // セルのイメージにタグを設定
+        cell.imageView?.tag = indexPath.row
+        
+        // アイコンタップ時の関数呼び出しの設定
+        let tapFavoIcon = UITapGestureRecognizer(target: self, action: #selector(self.tapFavoIcon))
+        cell.imageView?.isUserInteractionEnabled = true
+        cell.imageView?.addGestureRecognizer(tapFavoIcon)
+        
         return cell
     }
     
@@ -85,23 +90,27 @@ class AllScheduleVC: UIViewController, UITableViewDelegate, UITableViewDataSourc
         return items.count
     }
     
-     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    // セルの高さの設定
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 70
     }
     
-//    func Favo(){
-//        print("AllScheduleVC")
-//    }
-    
-    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
-    //        if segue.identifier == "toStall" {
-    //            let stallVC = segue.destination as! StallVC
-    //            stallVC.items = sender as! [JSON]
-    //        }
-    //    }
+    func tapFavoIcon(gestureRecognizer: UITapGestureRecognizer) {
+        // タップしたセルの行番号取得
+        let rowNum = gestureRecognizer.view?.tag
+        
+        // お気に入り登録されていればそれを解除、されてなければ登録
+        if userDefaults.bool(forKey: items[rowNum!]["id"].string!) == true {
+            userDefaults.set(false, forKey: items[rowNum!]["id"].string!)
+        } else {
+            userDefaults.set(true, forKey: items[rowNum!]["id"].string!)
+        }
+        // 各テーブルビューをリロード
+        AllScheduleVC.allScheduleTableView.reloadData()
+        FavoScheduleVC.favoScheduleTableView.reloadData()
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
 }
--- schfes_app_iOS/FavoScheduleVC.swift
+++ schfes_app_iOS/FavoScheduleVC.swift
@@ -1,5 +1,5 @@
 //
-//  FavoScheduleVC.swift
+//  AllScheduleVC.swift
 //  schfes_app_iOS
 //
 //  Created by FGO on 2017/09/23.
@@ -13,6 +13,17 @@ import SwiftyJSON
 
 class FavoScheduleVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    // お気に入り時の画像
+    let favoIcon = UIImage(named: "images/favo_icon.png")?.ResizeUIImage(width: 45, height: 45).withRenderingMode(.alwaysOriginal)
+    // 非お気に入り時の画像
+    let noFavoIcon = UIImage(named: "images/no_favo_icon.png")?.ResizeUIImage(width: 45, height: 45).withRenderingMode(.alwaysOriginal)
+    
+    // お気に入り登録しているかどうかの値を保持する変数
+    // アプリが落ちても値を保持できるUserDefaultsを使用
+    let userDefaults = UserDefaults.standard
+    
+    // AllScheduleVCからリロードできるようにするため、staticに設定
+    static let favoScheduleTableView: UITableView = UITableView()
     
     // itemsをJSONの配列と定義
     var items: [JSON] = []
@@ -20,39 +31,50 @@ class FavoScheduleVC: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // TableViewを作成
-        let tableView = UITableView()
-        tableView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-        tableView.delegate = self
-        tableView.dataSource = self
-        self.view.addSubview(tableView)
-        
-        let toStall = UITapGestureRecognizer(target: self, action: #selector(self.toStall))
-        tableView.addGestureRecognizer(toStall)
+        // TableViewの設定
+        FavoScheduleVC.favoScheduleTableView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+        FavoScheduleVC.favoScheduleTableView.delegate = self
+        FavoScheduleVC.favoScheduleTableView.dataSource = self
+        self.view.addSubview(FavoScheduleVC.favoScheduleTableView)
         
         // データを取得
         let listUrl = "http://ytrw3xix.0g0.jp/app2017/timetable";
         Alamofire.request(listUrl).responseJSON{ response in
             let json = JSON(response.result.value ?? "")
             json.forEach{(_, data) in
+                // itemsに取得したデータを追加
                 self.items.append(data)
             }
-            tableView.reloadData()
+            // テーブルビューのりろー度
+            FavoScheduleVC.favoScheduleTableView.reloadData()
         }
     }
     // Cellが選択された際に呼び出される
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Num: \(indexPath.row)")
-        print("Value: \(items[indexPath.row])")
+        if let cell = tableView.cellForRow(at: indexPath) {
+            // cellの選択を解除する
+            // これがないとセルを選択した時グレーになり続ける
+            cell.isSelected = false
+        }
     }
     
     // tableのcellにAPIから受け取ったデータを入れる
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "TableCell")
-        cell.textLabel?.text = items[indexPath.row]["title"].string
-        cell.detailTextLabel?.text = "開催時刻 : \(items[indexPath.row]["time"].stringValue)"
-        //        cell.textLabel?.text = items["timetable"][indexPath.row]["title"].string
-        //        cell.detailTextLabel?.text = "投稿日 : \(items[indexPath.row].stringValue)"
+        // お気に入りされている場合のみ、テキストを設定し、お気に入りイメージを追加する
+        if userDefaults.bool(forKey: items[indexPath.row]["id"].string!) == true {
+            cell.textLabel?.text = items[indexPath.row]["title"].string
+            cell.detailTextLabel?.text = "開催時刻 : \(items[indexPath.row]["time"].stringValue)"
+            cell.imageView?.image = favoIcon
+        }
+        // セルのイメージにタグを設定
+        cell.imageView?.tag = indexPath.row
+        
+        // アイコンタップ時の関数呼び出しの設定
+        let tapFavoIcon = UITapGestureRecognizer(target: self, action: #selector(self.tapFavoIcon))
+        cell.imageView?.isUserInteractionEnabled = true
+        cell.imageView?.addGestureRecognizer(tapFavoIcon)
+        
         return cell
     }
     
@@ -61,19 +83,31 @@ class FavoScheduleVC: UIViewController, UITableViewDelegate, UITableViewDataSour
         return items.count
     }
     
-    func toStall(){
-        print("FavoScheduleVC")
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if userDefaults.bool(forKey: items[indexPath.row]["id"].string!) == true {
+            return 70
+        } else {
+            return 0
+        }
     }
     
-    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
-    //        if segue.identifier == "toStall" {
-    //            let stallVC = segue.destination as! StallVC
-    //            stallVC.items = sender as! [JSON]
-    //        }
-    //    }
+    func tapFavoIcon(gestureRecognizer: UITapGestureRecognizer) {
+        // タップしたセルの行番号取得
+        let rowNum = gestureRecognizer.view?.tag
+        
+        // お気に入り登録されていればそれを解除、されてなければ登録
+        if userDefaults.bool(forKey: items[rowNum!]["id"].string!) == true {
+            userDefaults.set(false, forKey: items[rowNum!]["id"].string!)
+        } else {
+            userDefaults.set(true, forKey: items[rowNum!]["id"].string!)
+        }
+        // 各テーブルビューをリロード
+        AllScheduleVC.allScheduleTableView.reloadData()
+        FavoScheduleVC.favoScheduleTableView.reloadData()
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
 }
